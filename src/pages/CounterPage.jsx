b5b02abf5ea438adc3238da0703d5dd9047ee77c
numberwang55 <cban0901@hotmail.com>
2024-04-20 15:49:43 +0100

    update: CounterPage use form and stateful before useReducer refactor

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,12 +1,47 @@
-import Button from "../components/Button"
-import useCounter from "../hooks/use-counter"
+import { useState } from "react";
+import Button from "../components/Button";
+import Panel from "../components/Panel";
 
 export default function CounterPage({ initialCount }) {
+  const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
-  const {count, increment} = useCounter(initialCount)
-  
-  return <div>
-    <h1>Count is {count}</h1>
-    <Button onClick={increment} primary>Increment</Button>
-  </div>
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const handleChange = (event) => {
+    setValueToAdd(+event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setCount(count + valueToAdd)
+    setValueToAdd(0)
+  }
+
+  return (
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">Add a custom amount!</label>
+        <input
+          value={valueToAdd || ""}
+          onChange={handleChange}
+          aria-label="input box"
+          type="number"
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button>Add</Button>
+      </form>
+    </Panel>
+  );
 }
